7f9d2990a74d4f0ef00c6a20e563861fc1ffd917
milkpotatoes <chenshulin5@huawei.com>
2024-01-11 12:33:47 +0000

    !1 为搜索栏增加了一些功能 * Add command for searchbar * add support for search links

---
+++ js/utils/shortcutsFilter.js
@@ -0,0 +1,44 @@
+import { CustomShortcutsCollection } from "../customShortcutsCollection.js";
+
+export default class ShortcutFilter {
+    elem = document.querySelector('.shortcut-filter');
+    collection;
+    input = this.elem.querySelector('input');
+    /**
+     * 
+     * @param {CustomShortcutsCollection} collection 
+     */
+    constructor(collection) {
+        this.collection = collection;
+        this.input.addEventListener('input', e => {
+            this.filter(e.target.value);
+        });
+    }
+    focus() {
+        this.input.focus();
+    }
+    stretching() {
+        this.elem.style.transition = 'ease-in-out .2s';
+        this.elem.style.transform = 'translate(0, -20px)';
+        setTimeout(() => {
+            this.elem.style.transform = '';
+            setTimeout(() => {
+                this.elem.style.transition = '';
+            }, 200);
+        }, 200);
+    }
+    show() {
+        this.input.value = '';
+        this.elem.style.transition = '';
+        this.filter('');
+    }
+    hide() {
+        this.input.blur();
+        this.input.value = '';
+        this.filter('');
+    }
+    filter(key = '') {
+        this.collection.filter(key);
+        this.collection.refreshPageIndicator();
+    }
+}
